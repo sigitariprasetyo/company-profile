9a4e11661be8735b77acdd44d4294b1308d2d50f
Sigit Ari Prasetyo <sigitari111@gmail.com>
2019-06-10 08:21:56 +0700

    Menambahkan folder dan file baru.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav);
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    interval: 3000
+});
+
+var parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+
+const materialbox = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialbox);
+
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+    scrollOffset: 45
+});
